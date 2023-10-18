f58e9e6a2134f2befc28a78c1528a5d2e7385285
Joseph James <Joe.Centurion.7@gmail.com>
2023-10-18 17:04:42 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        int total = Integer.parseInt(args[0] + args[1]);
+        int total = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+
+        System.out.println(total + "\n\n\n\n\n\n\n\n\n\n");
+
         Threads[] threads = new Threads[total];
         Warehouse warehouse = new Warehouse();
-        for (int x = 0; x < total; x++) {
+        for (int x = 0; x <= total; x++) {
             threads[x] = new Threads(warehouse, Integer.parseInt(args[0]), x);
             threads[x].start();
         }
