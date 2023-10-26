e89beb606b3b365822f8bee55a4c7fb0ebc5ba5f
Joseph James <Joe.Centurion.7@gmail.com>
2023-10-26 18:26:29 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.lang.*;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         int total = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
 
-        System.out.println(total + "\n\n\n\n\n\n\n\n\n\n");
+        //System.out.println(total + "\n\n\n\n\n\n\n\n\n\n");
 
         Threads[] threads = new Threads[total];
         Warehouse warehouse = new Warehouse();
-        for (int x = 0; x <= total; x++) {
+        for (int x = 0; x <= total - 1; x++) {
             threads[x] = new Threads(warehouse, Integer.parseInt(args[0]), x);
             threads[x].start();
+            System.out.println("Number of threads..." + Thread.activeCount());
         }
     }
 }
